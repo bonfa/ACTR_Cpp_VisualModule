5cda235b0f816aad664c904154d8a9c996d16e28
Enrico <enrico@vgk1.(none)>
2012-10-01 16:35:02 +0200

    Tutorial Lena

---
+++ ocv/src/ocv.cpp
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <cv.h>
+#include <highgui.h>
+
+int main(int argc, char *argv[])
+{
+  IplImage* img = 0;
+
+  img=cvLoadImage("./lena.jpg");     // carica l'immagine
+
+  cvNamedWindow("mainWin", CV_WINDOW_AUTOSIZE);       // crea la finestra
+
+  cvShowImage("mainWin", img );    //  mostra l'immagine
+
+  cvWaitKey(0);    // wait for a key
+
+  cvReleaseImage(&img );    //rilascia l'immagine
+
+  system("PAUSE");
+  return 0;
+}
+

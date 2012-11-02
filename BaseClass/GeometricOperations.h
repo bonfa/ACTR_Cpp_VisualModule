b4b631e4f018b9fca2b02336e77860b3ab193fe8
bonfa <francesco.bonfadelli@gmail.com>
2012-11-02 11:47:04 +0100

    inserita la classe per le operazioni geometriche

---
+++ BaseClass/GeometricOperations.h
@@ -0,0 +1,18 @@
+/*
+ * GeometricOperations.h
+ *
+ *  Created on: 02/nov/2012
+ *      Author: francesco
+ */
+
+#ifndef GEOMETRICOPERATIONS_H_
+#define GEOMETRICOPERATIONS_H_
+
+#include "StraightLine.h"
+
+
+#endif /* GEOMETRICOPERATIONS_H_ */
+
+
+
+

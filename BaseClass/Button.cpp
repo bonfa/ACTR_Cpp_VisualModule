8b7971e1351a1231954fc8977216b968592b053b
Enrico Rizzardi <enrico.rizzardi@geemail.com>
2012-11-02 15:51:38 +0100

    Aggiunte eccezioni in button

--- BaseClass/Button.cpp
+++ BaseClass/Button.cpp
@@ -14,9 +14,10 @@ Button::~Button() {
 
 Button::Button(int x,int y, int height, int width, string txt) :text(""){
 	if(x< 0|| y < 0 || width <1 || height < 1)
-		printf("Eccezione"); //TODO
+		throw InputException(("Inammisible values in the points coordinates"));
 	if(txt.length()<1)
-		printf("Eccezione"); //TODO
+		throw InputException(("Text is empty"));
+
 	a = cvPoint(x, y);
 	b = cvPoint(x + width, y);
 	c = cvPoint(x + width, y+ height);

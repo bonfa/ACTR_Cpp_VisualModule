3067d1700f67bd3185c5d19e9013b314c30e6b3f
bonfa <francesco.bonfadelli@gmail.com>
2012-11-02 11:47:34 +0100

    inserito il corpo della classe per le operazioni geometriche

---
+++ BaseClass/GeometricOperations.cpp
@@ -0,0 +1,19 @@
+/*
+ * GeometricOperations.cpp
+ *
+ *  Created on: 02/nov/2012
+ *      Author: francesco
+ */
+
+
+#include "GeometricOperations.h"
+
+/*
+bool pointBelongsToStraightLine(int xp, int yp, double m, double q=0);
+
+bool pointBelongsToStraightLine(int xp, int yp, double a, double b, double c);
+
+ calculateStraightLineEquation(int xa,int ya, int xb, int yb);
+
+
+*/

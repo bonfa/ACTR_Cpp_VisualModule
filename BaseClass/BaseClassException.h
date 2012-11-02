f2ad8a8c9483d4b19f5978740744650fd1cc6fd1
bonfa <francesco.bonfadelli@gmail.com>
2012-11-02 11:46:14 +0100

    risolto il problema nel modulo BaseClassexception

--- BaseClass/BaseClassException.h
+++ BaseClass/BaseClassException.h
@@ -1,6 +1,7 @@
 // Exceptions
 #include <iostream>
 #include <exception>
+
 using namespace std;
 
 
@@ -13,7 +14,23 @@ class InputException: public exception
 
   virtual const char* what(string mex) const throw()
     {
-      return ("Input Exception - " + mex);
+	  return (string("Input Exception - ") + mex).c_str();
     }
+
 }inputException;
+
+
+
+class VerticalLineException: public exception
+{
+  virtual const char* what() const throw()
+  {
+    return "Vertical Stright Line Exception";
+  }
+
+  virtual const char* what(string mex) const throw()
+    {
+	  return (string("Input Exception - ") + mex).c_str();
+    }
+}verticalLineException;
 

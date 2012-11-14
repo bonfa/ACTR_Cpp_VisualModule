cce9c56e32f602e270e68f8741c36ace7181988c
Enrico Rizzardi <enrico.rizzardi@geemail.com>
2012-11-14 15:37:09 +0100

    Fatta serie di test con immagini acquisite da webcam

--- BaseClassTest/src/QRScannerTest.cpp
+++ BaseClassTest/src/QRScannerTest.cpp
@@ -25,8 +25,86 @@ void QRScannerTest01(){
 
 }
 
+void QRScannerTest02(){
+	QRScanner * qrs = new QRScanner("./qrcamtest.jpg");
+
+
+	string check = "Test";
+
+	ASSERT_EQUAL(qrs->getQRCode(),"QR-CodeTest");
+
+}
+
+void QRScannerTest03(){
+	QRScanner * qrs = new QRScanner("./qrcamtest2.jpg");
+
+
+
+
+	ASSERT_EQUAL(false,qrs->QRDetected());
+
+}
+
+void QRScannerTest04(){
+	QRScanner * qrs = new QRScanner("./qrcamtest3.jpg");
+
+
+	string check = "Test";
+
+	ASSERT_EQUAL("QR-CodeTest",qrs->getQRCode());
+
+}
+
+void QRScannerTest05(){
+	QRScanner * qrs = new QRScanner("./qrcamtest4.jpg");
+
+
+	string check = "Test";
+
+	ASSERT_EQUAL("QR-CodeTest",qrs->getQRCode());
+
+}
+
+
+void QRScannerTest06(){
+	QRScanner * qrs = new QRScanner("./qrcamtest5.jpg");
+
+
+	string check = "Test";
+
+	ASSERT_EQUAL("QR-CodeTest",qrs->getQRCode());
+
+}
+
+void QRScannerTest07(){
+	QRScanner * qrs = new QRScanner("./qrcamtest6.jpg");
+
+
+	string check = "Test";
+
+	ASSERT_EQUAL("QR-CodeTest",qrs->getQRCode());
+
+}
+
+void QRScannerTest08(){
+	QRScanner * qrs = new QRScanner("./qrcamtest7.jpg");
+
+
+	string check = "Test";
+
+	ASSERT_EQUAL(false,qrs->QRDetected());
+
+}
+
 cute::suite make_suite_QRScannerTest(){
 	cute::suite s;
 	s.push_back(CUTE(QRScannerTest01));
+	s.push_back(CUTE(QRScannerTest02));
+	s.push_back(CUTE(QRScannerTest03));
+	s.push_back(CUTE(QRScannerTest04));
+	s.push_back(CUTE(QRScannerTest05));
+	s.push_back(CUTE(QRScannerTest06));
+	s.push_back(CUTE(QRScannerTest07));
+	s.push_back(CUTE(QRScannerTest08));
 	return s;
 }

d423de88ac0a246ce2260152178b67acd53f369e
bonfa <francesco.bonfadelli@gmail.com>
2012-11-14 14:01:07 +0100

    RINOMINATI TEST PER TRIANGLE

--- BaseClassTest/src/TriangleTest.cpp
+++ BaseClassTest/src/TriangleTest.cpp
@@ -109,7 +109,7 @@ void TriangleTest_Error_06(){
 }
 
 
-void TriangleTest_Error_07(){
+void TriangleTest_ErrorAllineatedPoints_01(){
 	int ax = 1;
 	int ay = 2;
 	int bx = 3;
@@ -121,7 +121,7 @@ void TriangleTest_Error_07(){
 }
 
 
-void TriangleTest_Error_08(){
+void TriangleTest_ErrorCoincidentPoints_01(){
 	int ax = 1;
 	int ay = 1;
 	int bx = 1;
@@ -133,6 +133,39 @@ void TriangleTest_Error_08(){
 }
 
 
+void TriangleTest_ErrorCoincidentPoints_02(){
+	int ax = 1;
+	int ay = 1;
+	int bx = 1;
+	int by = 1;
+	int cx = 0;
+	int cy = 0;
+
+	ASSERT_THROWS(new Triangle(ax,ay,bx,by,cx,cy),InputException);
+}
+
+
+void TriangleTest_ErrorCoincidentPoints_03(){
+	int ax = 1;
+	int ay = 1;
+	int bx = 0;
+	int by = 0;
+	int cx = 1;
+	int cy = 1;
+
+	ASSERT_THROWS(new Triangle(ax,ay,bx,by,cx,cy),InputException);
+}
+
+void TriangleTest_ErrorCoincidentPoints_04(){
+	int ax = 0;
+	int ay = 0;
+	int bx = 1;
+	int by = 1;
+	int cx = 1;
+	int cy = 1;
+
+	ASSERT_THROWS(new Triangle(ax,ay,bx,by,cx,cy),InputException);
+}
 
 cute::suite make_suite_TriangleTest(){
 	cute::suite s;
@@ -144,8 +177,11 @@ cute::suite make_suite_TriangleTest(){
 	s.push_back(CUTE(TriangleTest_Error_04));
 	s.push_back(CUTE(TriangleTest_Error_05));
 	s.push_back(CUTE(TriangleTest_Error_06));
-	s.push_back(CUTE(TriangleTest_Error_07));
-	s.push_back(CUTE(TriangleTest_Error_08));
+	s.push_back(CUTE(TriangleTest_ErrorAllineatedPoints_01));
+	s.push_back(CUTE(TriangleTest_ErrorCoincidentPoints_01));
+	s.push_back(CUTE(TriangleTest_ErrorCoincidentPoints_02));
+	s.push_back(CUTE(TriangleTest_ErrorCoincidentPoints_03));
+	s.push_back(CUTE(TriangleTest_ErrorCoincidentPoints_04));
 	return s;
 }
 

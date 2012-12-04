39285517719237b2c01bf7ab178f7b70c92f2181
bonfa <francesco.bonfadelli@gmail.com>
2012-12-04 10:26:26 +0100

    inserito lo scheletro per l estrazione delle features

---
+++ BaseClass/src/FeatureExtractor.cpp
@@ -0,0 +1,18 @@
+/*
+ * FeatureExtractor.cpp
+ *
+ *  Created on: 04/dic/2012
+ *      Author: francesco
+ */
+
+#include "FeatureExtractor.h"
+
+FeatureExtractor::FeatureExtractor() {
+	// TODO Auto-generated constructor stub
+
+}
+
+FeatureExtractor::~FeatureExtractor() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ BaseClass/src/FeatureExtractor.h
@@ -0,0 +1,17 @@
+/*
+ * FeatureExtractor.h
+ *
+ *  Created on: 04/dic/2012
+ *      Author: francesco
+ */
+
+#ifndef FEATUREEXTRACTOR_H_
+#define FEATUREEXTRACTOR_H_
+
+class FeatureExtractor {
+public:
+	FeatureExtractor();
+	virtual ~FeatureExtractor();
+};
+
+#endif /* FEATUREEXTRACTOR_H_ */

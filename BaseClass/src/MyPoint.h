b3b5d4d4d3f22ef90142df22113997cc62d9df61
bonfa <francesco.bonfadelli@gmail.com>
2012-12-14 12:03:43 +0100

    inserita la documentazione per point

--- BaseClass/src/MyPoint.h
+++ BaseClass/src/MyPoint.h
@@ -3,6 +3,9 @@
  *
  *  Created on: 13/nov/2012
  *      Author: francesco
+ *
+ *  This class represents a 2D point.
+ *  It is defined by its x and y coordinates.
  */
 
 #ifndef MYPOINT_H_
@@ -15,8 +18,11 @@ public:
 	Point(int x,int y);
 	Point();
 	virtual ~Point();
+	/** Two points are equal when the x and the y coordinates are equal*/
 	bool operator==(const Point &other) const;
+	/** The x coordinate of the point*/
 	int x;
+	/** The y coordinate of the pont*/
 	int y;
 };
 

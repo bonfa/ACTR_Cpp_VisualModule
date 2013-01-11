4c90225f770c3aa17104b1923fabb4d139c2fa01
Enrico Rizzardi <enrico.rizzardi@gmail.com>
2013-01-11 13:11:45 +0100

    Aggiornato makefile e main.cpp

--- BaseClass/src/MarkerDetector.c
+++ BaseClass/src/MarkerDetector.c
@@ -5,30 +5,7 @@
 #define NO_IMG
 
 
-#ifdef _WIN32
-#  include <windows.h>
-#endif
-#include <stdio.h>
-#include <stdlib.h>
-#ifndef __APPLE__
-#  include <GL/glut.h>
-#else
-#  include <GLUT/glut.h>
-#endif
-#include <AR/gsub.h>
-#include <AR/video.h>
-#include <AR/param.h>
-#include <AR/ar.h>
-
-#define  OBJ1_PATT_NAME    "Data/patt.hiro"
-#define  OBJ2_PATT_NAME    "Data/patt.kanji"
-#define  OBJ1_SIZE         80.0
-#define  OBJ2_SIZE         80.0
-
-#define  OBJ1_MODEL_ID      1
-#define  OBJ2_MODEL_ID      2
-
-
+#include "MarkerDetector.h"
 
 
 typedef struct {
---
+++ BaseClass/src/MarkerDetector.h
@@ -0,0 +1,34 @@
+/*
+ * Input.h
+ *
+ *      Author: enrico
+ *
+ */
+
+#ifndef MDETECT_H_
+#define MDETECT_H_
+
+#ifdef _WIN32
+#  include <windows.h>
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+#ifndef __APPLE__
+#  include <GL/glut.h>
+#else
+#  include <GLUT/glut.h>
+#endif
+#include <AR/gsub.h>
+#include <AR/video.h>
+#include <AR/param.h>
+#include <AR/ar.h>
+
+#define  OBJ1_PATT_NAME    "Data/patt.hiro"
+#define  OBJ2_PATT_NAME    "Data/patt.kanji"
+#define  OBJ1_SIZE         80.0
+#define  OBJ2_SIZE         80.0
+
+#define  OBJ1_MODEL_ID      1
+#define  OBJ2_MODEL_ID      2
+
+#endif /* MDETECT_H_ */

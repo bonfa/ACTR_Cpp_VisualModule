671e3a2ce7e73ee3258340a097f8a9ff25d37c33
bonfa <francesco.bonfadelli@gmail.com>
2013-01-11 11:59:16 +0100

    inseriti un po di test per rect

--- BaseClassTest/src/RectTest.cpp
+++ BaseClassTest/src/RectTest.cpp
@@ -4,106 +4,197 @@
 #include "RectTest.h"
 
 
-void RectTest_Constructor_01(){
-	Rect *r = new Rect();
-	ASSERT(r!=NULL);
-}
+struct ConstructorError{
+	void ParameterError_01() {
+		int x = -1;
+		int y = 0;
+		int height = 1;
+		int width = 1;
+
+		ASSERT_THROWS(new Rect(x,y,height,width),InputException);
+	}
 
 
-void RectTest_Constructor_02(){
-	Rect *r = new Rect(4,4,4,4);
-	ASSERT(r!=NULL);
-}
+	void ParameterError_02() {
+		int x = 0;
+		int y = -1;
+		int height = 1;
+		int width = 1;
+
+		ASSERT_THROWS(new Rect(x,y,height,width),InputException);
+	}
 
 
-void RectTest_ParameterCheck_01() {
-	Rect *r = new Rect();
-	ASSERT_EQUAL(0,r->x);
-	ASSERT_EQUAL(0,r->y);
-	ASSERT_EQUAL(0,r->height);
-	ASSERT_EQUAL(0,r->width);
-}
+	void ParameterError_03() {
+		int x = 0;
+		int y = 0;
+		int height = 0;
+		int width = 1;
+
+		ASSERT_THROWS(new Rect(x,y,height,width),InputException);
+	}
 
 
-void RectTest_ParameterCheck_02() {
-	int x = 0;
-	int y = 0;
-	int height = 2;
-	int width = 1;
+	void ParameterError_04() {
+		int x = 0;
+		int y = 0;
+		int height = 1;
+		int width = 0;
 
-	Rect *r = new Rect(x,y,height,width);
-	ASSERT_EQUAL(x,r->x);
-	ASSERT_EQUAL(y,r->y);
-	ASSERT_EQUAL(height,r->height);
-	ASSERT_EQUAL(width,r->width);
-}
+		ASSERT_THROWS(new Rect(x,y,height,width),InputException);
+	}
+};
 
 
-void RectTest_ParameterError_01() {
-	int x = -1;
-	int y = 0;
-	int height = 1;
-	int width = 1;
+struct RectTest_01{
 
-	ASSERT_THROWS(new Rect(x,y,height,width),InputException);
-}
+	RectTest_01(): r(){
+		expectedPointList.push_back(Point(0,0));
+		expectedPointList.push_back(Point(0,0));
+		expectedPointList.push_back(Point(0,0));
+		expectedPointList.push_back(Point(0,0));
+	}
+
+	void Constructor(){
+		ASSERT_EQUAL(false, (&r)==NULL);
+	}
+
+	void checkX() {
+		ASSERT_EQUAL(0,r.x);
+	}
+
+	void checkY() {
+		ASSERT_EQUAL(0,r.y);
+	}
+
+	void checkHeight() {
+		ASSERT_EQUAL(0,r.height);
+	}
+
+	void checkWidth() {
+		ASSERT_EQUAL(0,r.width);
+	}
+
+	void checkEqual_01() {
+		Rect *r2 = new Rect();
+		ASSERT_EQUAL(r,*r2);
+	}
+
+	void checkEqual_02() {
+		Rect *r2 = new Rect();
+		ASSERT_EQUAL(true,r==*r2);
+	}
+
+	void checkEqual_03() {
+		Rect r2(5,5,5,5);
+		ASSERT_EQUAL(false,r==r2);
+	}
+
+	void checkGetPoints(){
+		std::vector<Point> pointList = r.getPoints();
+		ASSERT_EQUAL(expectedPointList,pointList);
+	}
+
+	Rect r;
+	std::vector<Point> expectedPointList;
+};
 
 
-void RectTest_ParameterError_02() {
-	int x = 0;
-	int y = -1;
-	int height = 1;
-	int width = 1;
+struct RectTest_02{
 
-	ASSERT_THROWS(new Rect(x,y,height,width),InputException);
-}
+	RectTest_02(): r(4,3,7,5){
+		expectedPointList.push_back(Point(4,3));
+		expectedPointList.push_back(Point(9,3));
+		expectedPointList.push_back(Point(9,10));
+		expectedPointList.push_back(Point(4,10));
+	}
+
+	void Constructor(){
+		ASSERT_EQUAL(false, (&r)==NULL);
+	}
+
+	void checkX() {
+		ASSERT_EQUAL(4,r.x);
+	}
+
+	void checkY() {
+		ASSERT_EQUAL(3,r.y);
+	}
+
+	void checkHeight() {
+		ASSERT_EQUAL(7,r.height);
+	}
+
+	void checkWidth() {
+		ASSERT_EQUAL(5,r.width);
+	}
+
+	void checkEqual_01() {
+		Rect *r2 = new Rect(4,3,7,5);
+		ASSERT_EQUAL(r,*r2);
+	}
+
+	void checkEqual_02() {
+		Rect *r2 = new Rect(4,3,7,5);
+		ASSERT_EQUAL(true,r==*r2);
+	}
+
+	void checkEqual_03() {
+		Rect r2(5,5,5,5);
+		ASSERT_EQUAL(false,r==r2);
+	}
+
+	void checkGetPoints(){
+		std::vector<Point> pointList = r.getPoints();
+		ASSERT_EQUAL(expectedPointList,pointList);
+	}
+
+	Rect r;
+	std::vector<Point> expectedPointList;
+};
 
 
-void RectTest_ParameterError_03() {
-	int x = 0;
-	int y = 0;
-	int height = 0;
-	int width = 1;
-
-	ASSERT_THROWS(new Rect(x,y,height,width),InputException);
-}
-
-
-void RectTest_ParameterError_04() {
-	int x = 0;
-	int y = 0;
-	int height = 1;
-	int width = 0;
-
-	ASSERT_THROWS(new Rect(x,y,height,width),InputException);
-}
-
-
-void RectTest_Operator_01() {
-	Rect *r1 = new Rect(4,4,4,4);
-	Rect *r2 = new Rect(4,4,4,4);
-	ASSERT_EQUAL(*r1,*r2);
-}
-
-void RectTest_Operator_02() {
-	Rect *r1 = new Rect();
-	Rect *r2 = new Rect();
-	ASSERT_EQUAL(*r1,*r2);
-}
 
 
 cute::suite make_suite_RectTest(){
 	cute::suite s;
-	s.push_back(CUTE(RectTest_Constructor_01));
+
+	s+= CUTE_SMEMFUN(ConstructorError,ParameterError_01);
+	s+= CUTE_SMEMFUN(ConstructorError,ParameterError_02);
+	s+= CUTE_SMEMFUN(ConstructorError,ParameterError_03);
+	s+= CUTE_SMEMFUN(ConstructorError,ParameterError_04);
+
+	s+= CUTE_SMEMFUN(RectTest_01,Constructor);
+	s+= CUTE_SMEMFUN(RectTest_01,checkX);
+	s+= CUTE_SMEMFUN(RectTest_01,checkY);
+	s+= CUTE_SMEMFUN(RectTest_01,checkHeight);
+	s+= CUTE_SMEMFUN(RectTest_01,checkWidth);
+	s+= CUTE_SMEMFUN(RectTest_01,checkEqual_01);
+	s+= CUTE_SMEMFUN(RectTest_01,checkEqual_02);
+	s+= CUTE_SMEMFUN(RectTest_01,checkEqual_03);
+	s+= CUTE_SMEMFUN(RectTest_01,checkGetPoints);
+
+	s+= CUTE_SMEMFUN(RectTest_02,Constructor);
+	s+= CUTE_SMEMFUN(RectTest_02,checkX);
+	s+= CUTE_SMEMFUN(RectTest_02,checkY);
+	s+= CUTE_SMEMFUN(RectTest_02,checkHeight);
+	s+= CUTE_SMEMFUN(RectTest_02,checkWidth);
+	s+= CUTE_SMEMFUN(RectTest_02,checkEqual_01);
+	s+= CUTE_SMEMFUN(RectTest_02,checkEqual_02);
+	s+= CUTE_SMEMFUN(RectTest_02,checkEqual_03);
+	s+= CUTE_SMEMFUN(RectTest_02,checkGetPoints);
+
+
+	/*
+	//s.push_back(CUTE(RectTest_Constructor_01));
 	s.push_back(CUTE(RectTest_Constructor_02));
-	s.push_back(CUTE(RectTest_ParameterCheck_01));
+	//s.push_back(CUTE(RectTest_ParameterCheck_01));
 	s.push_back(CUTE(RectTest_ParameterCheck_02));
-	s.push_back(CUTE(RectTest_ParameterError_01));
-	s.push_back(CUTE(RectTest_ParameterError_02));
-	s.push_back(CUTE(RectTest_ParameterError_03));
-	s.push_back(CUTE(RectTest_ParameterError_04));
+
 	s.push_back(CUTE(RectTest_Operator_01));
-	s.push_back(CUTE(RectTest_Operator_02));
+	//s.push_back(CUTE(RectTest_Operator_02));
+	 * */
+
 	return s;
 }
 

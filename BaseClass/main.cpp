cba1f62d8c3d34bf37168c621469e1d66ef4fc66
Enrico Rizzardi <enrico.rizzardi@geemail.com>
2013-01-17 14:42:43 +0100

    Spostate le costanti dei path in un header file

--- BaseClass/main.cpp
+++ BaseClass/main.cpp
@@ -25,30 +25,8 @@
 
 //# define IMG_PATH_01 "./shapes.png";
 //# define IMG_PATH_02 "./ellipses.jpg";
-# define IMG_PATH_01 "./images/01_02_1030797656577.bmp"
-# define IMG_PATH_02 "./images/02_08_447099175936.bmp"
-# define IMG_PATH_03 "./images/03_32_9007255098753112.bmp"
-# define IMG_PATH_04 "./images/04_36_1478332047688192.bmp"
-# define IMG_PATH_05 "./images/05_50_1548318539776144.bmp"
-# define IMG_PATH_06 "./images/06_54_18023194906722704.bmp"
-# define IMG_PATH_07 "./images/07_04_219903404277765.bmp"
-# define IMG_PATH_08 "./images/08_06_114392163942656.bmp"
-# define IMG_PATH_09 "./images/09_25_1337006283030553.bmp"
-# define IMG_PATH_10 "./images/10_26_1548114663047204.bmp"
-# define IMG_PATH_11 "./images/11_29_11261199174860888.bmp"
-# define IMG_PATH_12 "./images/12_53_145525316052451328.bmp"		//TODO quadrato esterno
-# define IMG_PATH_13 "./images/13_59_142118473433600.bmp"		//TODO	quadrato esterno
-# define IMG_PATH_14 "./images/14_NA_498222760961.bmp"
-# define IMG_PATH_15 "./images/15_NA_10793261728000.bmp"
-# define IMG_PATH_16 "./images/16_NA_211142744211593.bmp"
-# define IMG_PATH_17 "./images/17_NA_72480910316142593.bmp"		//TODO  quadrato esterno
-# define IMG_PATH_18 "./images/18_NA_577445915800635392.bmp"
-# define IMG_PATH_19 "./images/19_NA_580260668791324672.bmp"		//TODO	quadrato esterno
-# define IMG_PATH_20 "./images/20_NA_583224952673546240.bmp"
-# define IMG_PATH_21 "./images/21_NA_584350852048749056.bmp"
-# define IMG_PATH_22 "./images/22_NA_865966563026534912.bmp"
-# define IMG_PATH_23 "./images/23_NA_867083665762158848.bmp"
-# define IMG_PATH_24 "./images/24_M01_216770917518016516.bmp"
+
+#include "PathConstants.h"
 
 
 
